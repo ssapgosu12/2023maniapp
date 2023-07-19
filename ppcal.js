406aff2eea66e1e554a4b183a94e704e684b9ad5
ssapgosu <53387925+ssapgosu12@users.noreply.github.com>
2023-07-19 23:39:03 +0900

    Update ppcal.js

--- ppcal.js
+++ ppcal.js
@@ -13,7 +13,7 @@ document.getElementById("320").addEventListener("change", updatePP);
 function getStar() {
     var star = document.getElementById("star").value;
     //if none, return 0
-    if (star == "null") {
+    if (star == "") {
         return 0;}
     return star;
 }
@@ -21,22 +21,25 @@ function getStar() {
 function getAcc() {
     var acc = document.getElementById("acc").value;
     //if none, return 0
-    if (acc == "null") {
-        return 0;}
+    if (acc == "") {
+        return 0.95;}
     return acc;
 }
 //get notes value
 function getNotes() {
     var notes = document.getElementById("notes").value;
-    if (notes == "null") {
-        return 0;}
+    console.log(notes);
+    if (notes == "") {
+        console.log("notes is none");
+        return -1;}
     return notes;
 }
 //get 320 value
 function get320() {
     var three20 = document.getElementById("320").value;
-    if (three20 == "null") {
-        return 0;}
+    if (three20 == "") {
+        console.log("three20 is none");
+        return -1;}
     return three20;
 }
 
@@ -46,9 +49,29 @@ function updatePP() {
     console.log("updatePP");
     var star = getStar();
     var acc = getAcc();
-    if (acc >1 ){acc /= 100;}
+    if (acc > 1 ){acc /= 100;}
     var notes = getNotes();
     var three20 = get320();
+    if (notes == -1 && three20 == -1){
+        notes = 1500;
+        three20 = acc_to_ratio(acc)*notes;
+        document.getElementById("gnotes").innerHTML = "i guess it is" + notes.toFixed().toString();
+        document.getElementById("g320").innerHTML = "i guess it is" + three20.toFixed().toString();
+    }
+    else if (notes == -1){
+        notes = three20/acc_to_ratio(acc);
+        document.getElementById("gnotes").innerHTML = "i guess it is" + notes.toFixed().toString();
+        document.getElementById("g320").innerHTML = "";
+        
+    }
+    else if (three20 == -1){
+        three20 = acc_to_ratio(acc)*notes;
+        document.getElementById("g320").innerHTML = "i guess it is" + three20.toFixed().toString();
+        document.getElementById("gnotes").innerHTML = "";
+    }
+    else{
+        document.getElementById("gnotes").innerHTML = "";
+        document.getElementById("g320").innerHTML = "";
     var pp = calculatePP(star, acc, notes, three20);
     document.getElementById("pp").innerHTML = pp;
     console.log("star: " + star + " acc: " + acc + " notes: " + notes + " 320: " + three20 + " pp: " + pp);
@@ -59,5 +82,14 @@ function calculatePP(star, acc, notes, three20) {
     var maxpp = 8*Math.pow(Math.max(star - 0.15, 0.05), 2.2)  * (1 + 0.1 * Math.min(1, notes / 1500));
     var v2acc = acc*0.9375 + 0.0625*(three20/notes);
     var pp = maxpp * Math.max(0, 5 * v2acc - 4);
+    console.log(acc_to_ratio(acc), acc)
     return pp.toFixed(2);
+}
+
+//guess three20/notes
+function acc_to_ratio(acc){
+    acc *=100;
+    var ratio = 0.21951074725992828 + -1.8917388061679286/(acc-103.52761478506012)
+    console.log(ratio);
+    return ratio;
 }
